6b7e72cbdd452c532853aeccbdf1934683fcb2c2
stklug84 <45033201+stklug84@users.noreply.github.com>
2023-07-06 10:50:09 -0400

    Add Dockerfile

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,12 +4,16 @@ from translator import english_to_french, french_to_english
 class TestTranslateMethods(unittest.TestCase):
 
     def testEnglishToFrench(self):
-        self.assertEqual(english_to_french('Hello'),'Pepitoooo')
+        self.assertEqual(english_to_french('Hi'),'Bonjour')
+        self.assertNotEqual(english_to_french('Hello'),'Hi')
         self.assertEqual(english_to_french('Yes'),'Oui')
+        self.assertNotEqual(english_to_french('Yes'),'No')
 
     def testFrenchToEnglish(self):
         self.assertEqual(french_to_english('vin',),'wine')
+        self.assertNotEqual(french_to_english('vin',),'vino')
         self.assertEqual(french_to_english('moutarde'),'mustard')
+        self.assertNotEqual(french_to_english('moutarde',),'moustache')
 
 if __name__ == '__main__':
     unittest.main()
